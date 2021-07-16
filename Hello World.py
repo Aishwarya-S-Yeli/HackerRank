0b5c853d0ae0f06e91115851c002d42370dc0b81
Aishwarya-S-Yeli <86348860+Aishwarya-S-Yeli@users.noreply.github.com>
2021-07-16 22:44:12 +0530

    Hello World!

---
+++ Hello World.py
@@ -0,0 +1,11 @@
+# Task
+# To print "Hello World!" without assigning it to any string variable
+
+# Input format: You do not need to read any input in this challenge.
+# Output format: Print Hello, World! to stdout.
+# Sample Output: Hello, World!
+
+# CODE
+
+if __name__ == '__main__':
+    print "Hello, World!"
