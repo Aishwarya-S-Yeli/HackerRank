dccfbaaa9a6de49177e90039f2064a6dc6bf9ae4
Aishwarya-S-Yeli <86348860+Aishwarya-S-Yeli@users.noreply.github.com>
2021-07-18 21:47:32 +0530

    Square of numbers from 1 to 20

---
+++ Square-loop.py
@@ -0,0 +1,14 @@
+# Task
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n, print i square.
+
+# Input format
+# The first and only line contains the integer, n.
+
+# Output format
+# Print n lines, one corresponding to each i.
+
+# CODE
+
+n = int(raw_input())
+for i in range (0,n):
+  print i*i
